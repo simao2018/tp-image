8510199ee9347708493f5b1136c326f92b436e26
chancel <chancel.sim@outlook.fr>
2020-08-11 23:39:17 +0200

    Eppuration du code

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+//Initialisation des dependances
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const crypto = require('crypto');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); 
 const multer = require('multer');
 const GridFsStorage = require('multer-gridfs-storage');
 const Grid = require('gridfs-stream');
@@ -19,15 +20,19 @@ app.set('view engine','ejs');
 // URL Mongo
 
 const mongoUri = 'mongodb+srv://l1rmC89feM7yufZL:l1rmC89feM7yufZL@cluster0.wfyio.gcp.mongodb.net/tp-image?retryWrites=true&w=majority';
+
+// Connection a la DB mongo
+
 const conn = mongoose.createConnection(mongoUri);
 
 let gfs;
 
 conn.once('open',()=>{
-
     gfs = Grid(conn.db, mongoose.mongo);
     gfs.collection('uploads');
 });
+
+//configuration de GridFS
 
 const storage = new GridFsStorage({
     url: mongoUri,
@@ -49,7 +54,8 @@ const storage = new GridFsStorage({
   });
   const upload = multer({ storage });
 
-  // GET Load Index
+  //Middleware
+// GET Load Index
 app.get('/',(req,res)=>{
    // res.render('index');
    gfs.files.find().toArray((err, files)=> {
@@ -70,13 +76,13 @@ app.get('/',(req,res)=>{
     
 });
 
-    // POST Image
+    // POST Image Ajout d'une nouvelle image
 app.post('/upload',upload.single('file'), (req,res)=>{
-    //res.json({file: req.file});
+    
     res.redirect('/');
 
 });
-
+ // Affichage des images
 app.get('/image/:filename', (req, res)=>{
     gfs.files.findOne({ filename: req.params.filename },(err, file) =>{
         
@@ -86,7 +92,7 @@ app.get('/image/:filename', (req, res)=>{
         }
     });
 });
-
+// Route delete
 app.delete('/files/:id', (req, res)=>{
     gfs.remove({_id: req.params.id, root: 'uploads'}, (err, gridStore)=>{
         if(err){
